03e1a1e42724c5056ea7485fa4816c742c4fc121
Jenna Jones <jljones121@gmail.com>
2024-03-05 02:33:55 +0000

    new.code.no.printf

--- printf.c
+++ printf.c
@@ -1,55 +1,45 @@
 #include <stdio.h>
+#include <stddef.h>
 #include <stdarg.h>
 #include "main.h"
 
-int _printf(const char *format, ...) {
+int _printf(const char *format, ...) 
+{
     va_list args;
     int count = 0;
 
     va_start(args, format);
 
     while (*format) {
-        if (*format == '%') {
+        if (*format == '%') 
+	{
             format++;
-            switch (*format) {
+            switch (*format) 
+	    {
                 case 'c':
-                    putchar(va_arg(args, int));
                     count++;
+                    va_arg(args, int);
                     break;
                 case 's':
-                    count += printf("%s", va_arg(args, const char *));
+                    count += vsnprintf(NULL, 0, va_arg(args, const char *), args);
                     break;
 		case 'd':
 		case 'i':
-		    count += printf("%d", va_arg(args, int));
-		    break;
 		case 'u':
-		    count += printf("%u", va_arg(args, unsigned int));
-		    break;
 		case 'o':
-		    count += printf("%o", va_arg(args, unsigned int));
-		    break;
-                case 'x':
-                    count += printf("%x", va_arg(args, unsigned int));
-                    break;
-                case 'X':
-                    count += printf("%X", va_arg(args, unsigned int));
-                    break;
+		case 'x':
+		case 'X':
 		case 'p':
-		    count += printf("%p", va_arg(args, void *));
-		    break;
+                    count += vsnprintf(NULL, 0, "", args);
+                    break;
                 case '%':
-		   putchar('%');
                     count++;
                     break;
                 default:
-		    putchar('%');
-                    putchar(*format);
                     count += 2;
                     break;
             }
         } else {
-            putchar(*format);
             count++;
         }
         format++;
